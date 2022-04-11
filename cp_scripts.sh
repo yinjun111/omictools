147d33af00dac67d669f7c331eb2ed2a5253fa59
Jun Yin <jun.yin@ferring.com>
2022-04-11 15:03:46 -0700

    update cp_scripts.sh

--- cp_scripts.sh
+++ cp_scripts.sh
@@ -1,7 +1,7 @@
 #synchronize scripts in the dev codes folder with the running folder
-rsync -a -d --delete /home/centos/Pipeline/omictools/ /apps/omictools/
+rsync -a -d --delete /home/jyin/Pipeline/omictools/ /apps/omictools/
 
-/usr/local/bin/aws s3 sync /home/centos/Pipeline/omictools/ s3://ferring-omictools/omictools --delete
+/usr/local/bin/aws s3 sync /home/jyin/Pipeline/omictools/ s3://ferring-omictools/omictools --delete
 
 #make everything execuable +x
 chmod 755 -R /apps/omictools/
